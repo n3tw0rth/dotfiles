e274ee3bd7cc0ad8d6bbcb3c502d4b93deee03d2
theekshana fernando <theekshana.fernando@surge.global>
2024-12-04 08:11:15 +0530

    chore: starship and nvim

--- .config/nvim/lua/options.lua
+++ .config/nvim/lua/options.lua
@@ -1,8 +1,7 @@
 require "nvchad.options"
 
--- add yours here!
-
--- local o = vim.o
--- o.cursorlineopt ='both' -- to enable cursorline!
+local o = vim.o
+o.so = 999
+-- o.cursorlineopt = 'both' -- to enable cursorline!
 vim.opt.relativenumber = true
 vim.opt.guicursor = "n-v-c-sm:block,i-ci-ve:ver25-Cursor,r-cr-o:hor20"
