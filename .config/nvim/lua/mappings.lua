45271e3bb1d7a6d214381e84e53e295bce304852
n3tw0rth <40062006+n3tw0rth@users.noreply.github.com>
2024-12-26 15:59:39 +0530

    chore: live grep dotfiles

--- .config/nvim/lua/mappings.lua
+++ .config/nvim/lua/mappings.lua
@@ -15,6 +15,14 @@ map({ "n", "v" }, "<leader>ck", [[:s/\v-([a-z])/_\1/g<CR>]], { desc = "kebab-cas
 -- telescope
 map("n", "<leader>fr", require("telescope.builtin").resume, { desc = "resume the previous picker" })
 map("n", "<leader>fgb", require("telescope.builtin").git_branches, { desc = "git branches" })
+map("n", "<leader>fm", require("telescope.builtin").marks, { desc = "show marks" })
+map("n", "<leader>fs", require("telescope.builtin").grep_string, { desc = "grep string under cursor" })
+map(
+  "n",
+  "<leader>fw",
+  ":lua require('telescope.builtin').live_grep({ additional_args = function() return { '--hidden' } end })<cr>",
+  { silent = true, desc = "Find in Files" }
+)
 
 -- Oil
 map("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
