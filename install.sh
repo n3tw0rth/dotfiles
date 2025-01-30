8dceb9188112887979282f515ba488a1a558f2fc
n3tw0rth <rtheekshanafernando@gmail.com>
2025-01-30 21:59:49 +0530

    chore: install script and stow ignore

---
+++ install.sh
@@ -0,0 +1,40 @@
+brew install stow
+brew install neovim
+brew install starship
+brew install tmux
+brew install lazygit
+brew install imagemagick
+brew install derailed/k9s/k9s
+
+sudo apt isntall i3-wm
+sudo apt install suckless-tools
+sudo apt install polybar
+sudo apt install keychain
+sudo apt install xclip
+sudo apt install maim
+sudo apt install snapd
+sudo apt install dunst
+
+# terminator
+sudo add-apt-repository ppa:mattrose/terminator
+sudo apt-get update
+sudo apt install terminator
+
+sudo apt install feh
+sudo apt install fzf
+
+sudo snap install aws-cli --classic
+sudo snap install dbeaver-ce
+sudo snap install kubectl --classic
+
+
+# install picom from source
+# next branch
+# https://github.com/yshui/picom
+
+#install i3lock-color from source 
+# https://github.com/Raymo111/i3lock-color
+
+
+
+sudo apt-get install pkg-config libssl-dev
