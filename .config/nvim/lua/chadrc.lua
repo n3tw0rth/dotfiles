054c88cf1840715012e79c98e1f7eea684eae45c
n3tw0rth <rtheekshanafernando@gmail.com>
2025-07-27 21:50:55 +0530

    updates

--- .config/nvim/lua/chadrc.lua
+++ .config/nvim/lua/chadrc.lua
@@ -8,27 +8,14 @@ M.ui = {
   telescope = {
     style = "bordered"
   },
+  cmp = {
+    style = "default"
+  },
 }
 
 M.base46 = {
   theme = "monochrome",
-
   transparency = true,
-
-  hl_override = {
-    Comment = { italic = true },
-    ["@comment"] = { italic = true },
-    NvimTreeNormal = { fg = '#9AA6B2' },
-    NvimTreeFolderName = { fg = '#9AA6B2' },
-    NvimTreeOpenedFolderName = { fg = '#9AA6B2' },
-    NvimTreeRootFolderName = { fg = '#9AA6B2' },
-    NvimTreeSymlink = { fg = '#9AA6B2' },
-    NvimTreeExecFile = { fg = '#9AA6B2' },
-    NvimTreeFile = { fg = '#9AA6B2' },
-    NvimTreeIndentMarker = { fg = '#9AA6B2' },
-    NvimTreeFolderIcon = { fg = '#9AA6B2' },
-    NvimTreeCursorLine = { fg = '#9AA6B2' },
-  },
 }
 
 return M
