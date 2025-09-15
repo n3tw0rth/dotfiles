69e55722778740014435da1e7e094fd7e59978a3
n3tw0rth <rtheekshanafernando@gmail.com>
2025-09-15 11:56:16 +0530

    chore(vim): updated the relative line number color

--- .config/nvim/lua/options.lua
+++ .config/nvim/lua/options.lua
@@ -3,3 +3,7 @@ require "nvchad.options"
 vim.opt.relativenumber = true
 vim.opt.guicursor = "n-v-c-sm:block,i-ci-ve:ver25-Cursor,r-cr-o:hor20"
 vim.opt.list = false
+
+vim.api.nvim_set_hl(0, "LineNrAbove", { fg = "#9AA6B2", bold = false })
+vim.api.nvim_set_hl(0, "LineNrBelow", { fg = "#9AA6B2", bold = false })
+vim.api.nvim_set_hl(0, "CursorLineNr", { fg = "white", bold = false })
