b63016dd271491ebeff09192c23a1d1386fb0895
n3tw0rth <rtheekshanafernando@gmail.com>
2025-12-26 15:09:26 +0530

    chore: updated lspconfig

--- .config/nvim/lua/configs/lspconfig.lua
+++ .config/nvim/lua/configs/lspconfig.lua
@@ -1,48 +1,47 @@
-local on_attach = require("nvchad.configs.lspconfig").on_attach
-local on_init = require("nvchad.configs.lspconfig").on_init
-local capabilities = require("nvchad.configs.lspconfig").capabilities
-
 local lspconfig = vim.lsp.config
-local servers = { "html", "cssls", "phpactor", "ts_ls", "pyright", "clangd", "terraformls", "gopls", "astro", "asm_ls",
-  "tailwindcss", "yamlls", "marksman" }
-
-vim.lsp.enable('marksman')
-vim.lsp.enable('rust_analyzer')
-
--- lsps with default config
-for _, lsp in ipairs(servers) do
-  lspconfig[lsp] = {
-    on_attach = on_attach,
-    on_init = on_init,
-    capabilities = capabilities,
-  }
-end
-
---rust
-lspconfig.rust_analyzer = {
-  on_attach = on_attach,
-  on_init = on_init,
-  filetypes = { "rust" },
-  settings = {
-    ["rust-analyzer"] = {
-      imports = {
-        granularity = {
-          group = "module",
+local servers = {
+  html = {},
+  cssls = {},
+  phpactor = {},
+  ts_ls = {},
+  pyright = {},
+  clangd = {},
+  terraformls = {},
+  gopls = {},
+  astro = {},
+  asm_ls = {},
+  tailwindcss = {},
+  yamlls = {},
+  marksman = {},
+  rust_analyzer = {
+    filetypes = { "rust" },
+    settings = {
+      ["rust-analyzer"] = {
+        imports = {
+          granularity = {
+            group = "module",
+          },
+          prefix = "self",
         },
-        prefix = "self",
-      },
-      cargo = {
-        allFeatures = true,
-        buildScripts = {
+        cargo = {
+          allFeatures = true,
+          buildScripts = {
+            enable = true,
+          },
+        },
+        procMacro = {
           enable = true,
         },
       },
-      procMacro = {
-        enable = true,
-      },
     },
   },
+  harper_ls = {}
 }
+
+for name, opts in pairs(servers) do
+  vim.lsp.config(name, opts)
+  vim.lsp.enable(name)
+end
 
 vim.api.nvim_create_autocmd({ "BufWritePre" }, {
   pattern = { "*.tf", "*.tfvars" },
